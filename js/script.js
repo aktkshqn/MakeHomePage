9c769c9ab0cf5c7e5f380d69255de6d062538689
Masa <masanobu827@gmail.com>
2025-12-10 22:43:09 +0900

    popupアニメーションを制作

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 "use-strict";
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry =>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('animation');
+
+            observer.unobserve(entry.target);
+        }
+    })
+},{
+    threshold: 0.1
+});
 window.addEventListener('DOMContentLoaded',()=>{
-    console.log("JavaScript is worked");  
+
+    const targetElements = document.querySelectorAll('.popup');
+    targetElements.forEach(el =>{
+        observer.observe(el);
+    });
 });
